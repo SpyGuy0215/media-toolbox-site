739d51f536102aaa3e7d6b9f3739eb49c907d789
Shashank Prasanna <shashankprasanna1@gmail.com>
2025-10-01 19:00:26 -0400

    added github sidebar buttons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import SidebarButton from './SidebarButton';
-import { LuAudioLines, LuHouse, LuRefreshCw } from "react-icons/lu";
+import { LuAudioLines, LuHouse, LuRefreshCw, LuGithub, LuBug } from "react-icons/lu";
 import {Separator} from "@/components/ui/separator";
+import {Button} from "@/components/ui/button";
 
 export default function Sidebar() {
     return(
@@ -11,8 +12,26 @@ export default function Sidebar() {
             <SidebarButton icon={<LuHouse />} className={'mt-4 py-6'} url={'/'}>Home</SidebarButton>
             <SidebarButton icon={<LuRefreshCw />} className={'py-6'} url={'/convert'}>Convert Files</SidebarButton>
             <SidebarButton icon={<LuAudioLines />} className={'py-6'} url={'/transcribe'}>Transcribe Audio</SidebarButton>
-            <div id={'sidebar-footer'} className={'mt-auto'}>
+            <div id={'sidebar-footer'} className={'mt-auto flex flex-col'}>
                 <Separator className={'mb-4'}/>
+                <Button className={'mb-2 w-2/3 mx-auto'}
+                onClick={() => window.open('https://github.com/SpyGuy0215/media-toolbox-site')}
+                >
+                    <LuGithub />
+                    Website
+                </Button>
+                <Button className={'mb-2 w-2/3 mx-auto'}
+                        onClick={() => window.open('https://github.com/SpyGuy0215/media-toolbox-site')}
+                >
+                    <LuGithub />
+                    Server
+                </Button>
+                <Button className={'mb-10 w-2/3 mx-auto bg-blue-500 hover:bg-blue-600'}
+                        onClick={() => window.open('https://github.com/SpyGuy0215/media-toolbox/issues')}
+                >
+                    <LuBug />
+                    Report a Bug
+                </Button>
                 <p id={'version'} className={'text-sm text-gray-500 mb-2 text-mono'}>v1.1.0</p>
                 <p className={'text-sm text-gray-500'}>Made by Shashank Prasanna</p>
             </div>
